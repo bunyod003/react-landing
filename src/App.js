2c1c12972433533c1d15a8c31322513015cc9130
bunyod003 <118340467+bunyod003@users.noreply.github.com>
2024-10-22 17:08:46 +0500

    Четвёртый коммит

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Service from './components/Service';
-import Contact from './components/Contact';
+import Home from './Home';
+import About from './About';
+import Service from './Service';
+import Contact from './Contact';
 
-// const Home = () => <div><h2>Home Page</h2><p>Welcome to our homepage!</p></div>;
-// const About = () => <div><h2>About Us</h2><p>Learn more about us here.</p></div>;
-// const Services = () => <div><h2>Our Services</h2><p>Details about our services.</p></div>;
-// const Contact = () => <div><h2>Contact Us</h2><p>Get in touch with us here.</p></div>;
 
 function App() {
     const [theme, setTheme] = useState('light');
@@ -30,7 +26,7 @@ function App() {
                 <Navbar toggleTheme={toggleTheme} currentTheme={theme} />
                 <div className="container mt-4">
                     <Routes>
-                        <Route basename="/react-landing" element={<Home />} />
+                        <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/services" element={<Service />} />
                         <Route path="/contact" element={<Contact />} />
